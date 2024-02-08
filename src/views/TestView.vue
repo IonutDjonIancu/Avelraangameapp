40f4764f57b948a35bba6d8a4b95f183b18d40ff
iancu ionut <iiancu85@gmail.com>
2024-02-08 13:01:19 +0100

    Added export players functionality.

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,13 +1,74 @@
 <template>
-  <h3 style="color: red; font-weight: bold">some testing stuff below</h3>
-  <hr />
-  <div style="display: flex; flex-direction: row; justify-content: center">
-    <!-- insert testing stuff below -->
-    <!-- insert testing stuff below -->
-    <!-- insert testing stuff below -->
+  <div class="container">
+    <hr />
+    <div class="column container container-b">
+      <!-- insert testing stuff below -->
+      <!-- insert testing stuff below -->
+      <!-- insert testing stuff below -->
+      <p>Export</p>
+      <form name="exportForm" class="row">
+        <label for="exportPass" class="m-h">pass</label>
+        <input
+          v-model="exportPass"
+          name="exportPassInput"
+          id="exportPass"
+          placeholder="speak friend and enter"
+          class="m-h"
+        />
+        <label for="exportSecret" class="m-h">secret</label>
+        <input
+          v-model="exportSecret"
+          name="exportSecretInput"
+          id="exportSecret"
+          placeholder="speak friend and enter"
+          class="m-h"
+        />
+        <button @click.prevent="exportSnapshot" type="submit" class="m-h">
+          submit
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { HttpService } from "@/services/HttpService";
+import { DbRequestsInfo } from "@/dtos/Dtos";
 
-<style scoped></style>
+const exportPass = ref<string>("");
+const exportSecret = ref<string>("");
+
+const exportSnapshot = () => {
+  const data: DbRequestsInfo = {
+    password: exportPass.value,
+    secret: exportSecret.value,
+    isShortOperation: true,
+  };
+
+  HttpService.httpPut("Database/ExportSnapshot", data)
+    .then((s) => s.text())
+    .then(console.log)
+    .catch((err) => console.error(err.message));
+};
+</script>
+
+<style scoped>
+.m-h {
+  margin-left: 2px;
+  margin-right: 2px;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.container-b {
+  width: 80%;
+  border: 2px solid darkgray;
+  margin: 5px;
+  border-radius: 5px;
+}
+</style>
